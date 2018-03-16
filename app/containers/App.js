1c49e9f0b72087694264f1439a1f54afea410645
dsm <diogenes.sazo@gmail.com>
2018-03-16 11:33:38 -0300

    First commit

---
+++ app/containers/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
+import { View, ScrollView, StyleSheet, TextInput, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { Actions } from 'react-native-router-flux'
+
+import { actionCreators } from '../redux/todoRedux'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
+
+const mapStateToProps = (state) => ({
+  items: state.items,
+})
+
+class App extends Component {
+
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(App)
